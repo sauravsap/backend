cd55c7a4101bff7dbe153ea63ee9ce4e9239cee8
saurav-sapkota <saurav4721@gmail.com>
2025-01-13 15:52:07 +0545

    another one

--- src/index.js
+++ src/index.js
@@ -1,23 +1,11 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 5000;
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-app.get('/login', (req, res) => {
-    res.send('Please Login')
-})
-app.get('/facebook', (req, res) => {
-    res.send('Wellcome to Facebook page')
-})
-app.get("/youtube", (req, res) => {
-    res.send('<h1> Wellcome to my youtube page </h1>')
-})
-app.get('/instagram', (req, res) => {
-    res.send('hello people')
-})
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+import mongoose from "mongoose";
+import {DB_NAME} from "./constants";
+import express from "express";
+( async ()=> {
+    try {
+       await mongoose.connect(`$(process.env.MONGODB_URI)`) 
+    } catch (error) {
+      console.error("Error:",error)
+      throw err  
+    }
+})()
